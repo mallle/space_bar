34c9460c6c306425865c2efd8bee2a6a0a44c085
Malene Klit <maleneklit@hotmail.com>
2019-07-22 09:00:25 +0200

    first 3 videos

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first page');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf('Fure page to show the article: %s', $slug));
+    }
+}
