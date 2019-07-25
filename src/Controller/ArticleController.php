5f346fbfa065fc105d18bd3669b008241483bf84
Malene Klit <maleneklit@hotmail.com>
2019-07-25 11:32:43 +0200

    video 1-4 Symfony 4 Fundamentals Services, Config and Environments

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,11 +1,11 @@
 <?php
-
 namespace App\Controller;
-
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Michelf\MarkdownInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class ArticleController extends AbstractController
 {
@@ -20,7 +20,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug)
+    public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
     {
         $comments = [
             'I ate a normal rock noce. It did NOT taste like bacon!',
@@ -28,10 +28,38 @@ class ArticleController extends AbstractController
             'Why should they taste like bacon?'
         ];
 
-        //dump($slug, $this);
+        $articleContent = <<<EOF
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+labore minim **pork belly** spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+turkey shank eu pork belly meatball non cupim.
+
+Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
+laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
+capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
+picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
+occaecat lorem meatball prosciutto quis strip steak.
+
+Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
+mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
+strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
+cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
+fugiat.
+EOF;
+
+        $item = $cache->getItem('markdown_'.md5($articleContent));
+        if(!$item->isHit()){
+            $item->set($markdown->transform($articleContent));
+            $cache->save($item);
+        }
+
+        $articleContent = $item->get();
+        dump($markdown);die;
+
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'articleContent' => $articleContent,
             'slug' => $slug,
             'comments' => $comments
         ]);
@@ -46,7 +74,7 @@ class ArticleController extends AbstractController
 
         $logger->info('Article is being hearted');
         //return new JsonResponse(['hearts' => rand(5,100)]);
-        return $this->json(['hearts' => rand(5,100)]);
+        return $this->json(['hearts' => rand(5, 100)]);
 
     }
 
