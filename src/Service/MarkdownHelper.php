3058ed6cc6c9afdbcb80d78e6fa767080395b336
Malene Klit <maleneklit@hotmail.com>
2019-07-25 16:46:28 +0200

    bis video 12 symfony 4 fundamentals, services, Confing and Environments

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -11,12 +11,14 @@ class MarkdownHelper
     private $cache;
     private $markdown;
     private $logger;
+    private $isDebug;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger)
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger, bool $isDebug)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
         $this->logger = $markdownLogger;
+        $this->isDebug = $isDebug;
     }
 
     public function parse(string $source): string
@@ -24,13 +26,15 @@ class MarkdownHelper
         if (stripos($source, 'bacon') !== false) {
             $this->logger->info('They are talking about bacon again!');
         }
-        
-        $item = $this->cache->getItem('markdown_'.md5($source));
-        if(!$item->isHit()){
-            $item->set($this->markdown->transform($source));
-            $this->cache->save($item);
+        if ($this->isDebug) {
+            $item = $this->cache->getItem('markdown_'.md5($source));
+            if(!$item->isHit()){
+                
+                    return $this->markdown->transform($source);
+                
+                $this->cache->save($item);
+            }
         }
-        
         return $item->get();
     }
 
