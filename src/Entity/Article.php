760b0367c7cd8bbe45c3081a2b475a6125e045e1
Malene Klit <maleneklit@hotmail.com>
2019-07-29 17:00:40 +0200

    video 17 and 18 - when migrations Fail and Activationg Timestampable

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,13 +4,15 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
-
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
  */
 class Article
 {
+    use TimestampableEntity;
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -153,4 +155,5 @@ class Article
         
         return $this;
     }
+
 }
