00b292bd948faab356e4885146a103c46e12404c
Malene Klit <maleneklit@hotmail.com>
2019-07-30 16:11:06 +0200

    awesome Random fixture

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -57,19 +57,6 @@ EOF
                 ->setImageFilename($this->faker->randomElement(self::$articleImages))
             ;
 
-            $comment1 = new Comment();
-            $comment1->setAuthorName('Mike Ferengi');
-            $comment1->setContent('I ate a normal rock once. It did NOT taste like bacon!');        
-            $comment1->setArticle($article);
-            $manager->persist($comment1);
-
-            $comment2 = new Comment();
-            $comment2->setAuthorName('Mike Ferengi');
-            $comment2->setContent('I like bacon sooo much');        
-            $comment2->setArticle($article);
-            $manager->persist($comment2);
-
-
         });
 
         $manager->flush();
---
+++ src/DataFixtures/CommentFixture.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use App\Entity\Comment;
+use App\Entity\Article;
+
+class CommentFixture extends BaseFixture implements DependentFixtureInterface
+{
+    public function loadData(ObjectManager $manager)
+    {
+        $this->createMany(Comment::class, 100, function(Comment $comment){
+            $comment->setContent(
+                $this->faker->boolean ? $this->faker->paragraph : $this->faker->sentences(2, true)
+            );
+            $comment->setAuthorName($this->faker->name);
+            $comment->setCreatedAt($this->faker->dateTimeBetween('-1 months', '-1 seconds'));
+
+            $comment->setArticle($this->getRandomReference(Article::class));
+
+        });
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [ArticleFixtures::class];
+    }
+}
