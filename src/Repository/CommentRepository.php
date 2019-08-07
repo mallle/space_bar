4abc3f4c29e4429e2a2df9f8bb0cedb0d4efef1f
Malene Klit <maleneklit@hotmail.com>
2019-08-07 09:04:29 +0200

    Video 15 - Pagination

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Comment;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 use Doctrine\Common\Collections\Criteria;
 
@@ -34,7 +35,7 @@ class CommentRepository extends ServiceEntityRepository
      * @param string|null $term
      * @return Comment[]
      */
-    public function findAllWithSearch(?string $term){
+    public function getWithSearchQueryBuilder(?string $term): QueryBuilder{
 
         $qb = $this->createQueryBuilder('c')
             ->innerJoin('c.article', 'a')
@@ -46,9 +47,7 @@ class CommentRepository extends ServiceEntityRepository
         }
 
         return $qb
-            ->orderBy('c.createdAt', 'DESC')
-            ->getQuery()
-            ->getResult();
+            ->orderBy('c.createdAt', 'DESC');
     }
 
 
